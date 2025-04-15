9bee02e9ae5d7a9902fd2c1f15637003db11fb46
hanna-uler <hannauler@gmail.com>
2025-04-15 14:24:50 -0400

    start structure creation, add first components

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import Product from "./Product";
+
+// function Product() {
+// 	return (
+// 		<div>
+// 			<h2>Cookies</h2>
+// 			<p>Price: 999 credits</p>
+// 		</div>
+// 	);
+// };
+
+export default function App() {
+	return (
+		<div>
+			<h1>Best selling</h1>
+			<Product />
+ 			<Product />
+ 			<Product />
+ 		</div>
+ 	);
+}
